a3153f827fdecfd9aaadbd010ee665ab077629ac
Kristen Picard <kristenpicard93@gmail.com>
2021-05-27 14:04:04 -0400

    Working on offline part of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,56 +12,64 @@ const FILES_TO_CACHE = [
 
 // ********************Got from mini project
 
-// const PRECACHE = "precache-v1";
-// const RUNTIME = "runtime";
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches
-//       .open(PRECACHE)
-//       .then((cache) => cache.addAll(FILES_TO_CACHE))
-//       .then(self.skipWaiting())
-//   );
-// });
+// Caches list of files above
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
+});
 
 // The activate handler takes care of cleaning up old caches.
-// self.addEventListener("activate", (event) => {
-//   const currentCaches = [PRECACHE, RUNTIME];
-//   event.waitUntil(
-//     caches
-//       .keys()
-//       .then((cacheNames) => {
-//         return cacheNames.filter(
-//           (cacheName) => !currentCaches.includes(cacheName)
-//         );
-//       })
-//       .then((cachesToDelete) => {
-//         return Promise.all(
-//           cachesToDelete.map((cacheToDelete) => {
-//             return caches.delete(cacheToDelete);
-//           })
-//         );
-//       })
-//       .then(() => self.clients.claim())
-//   );
-// });
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
 
-// self.addEventListener("fetch", (event) => {
-//   if (event.request.url.startsWith(self.location.origin)) {
-//     event.respondWith(
-//       caches.match(event.request).then((cachedResponse) => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-//         return caches.open(RUNTIME).then((cache) => {
-//           return fetch(event.request).then((response) => {
-//             return cache.put(event.request, response.clone()).then(() => {
-//               return response;
-//             });
-//           });
-//         });
-//       })
-//     );
-//   }
-// });
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+
+    // If the request is not for the API, serve static assets offline
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
